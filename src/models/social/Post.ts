f88ea14cd998044be23155079fecee5922a57a00
Gcurrivand <114084438+Gcurrivand@users.noreply.github.com>
2023-01-17 13:59:04 +0100

    Gcu/fc/service social (#6)
    * Post et evenment model et service post
    
    * Post et evenment model et service post
    
    Co-authored-by: gcurriva <gkurvanov@outlook.fr>

---
+++ src/models/social/Post.ts
@@ -0,0 +1,34 @@
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne} from 'typeorm'
+import {JoinColumn} from "typeorm/browser";
+import {Event} from "./Evenement";
+
+
+@Entity('posts')
+export class Post {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    titre!: string
+
+    @Column()
+    description!: string
+
+    @Column()
+    estPartage!: boolean
+
+    @Column()
+    idUtilisateur!: string
+
+    @OneToOne(() => Event) @JoinColumn()
+    idEvenement!: number
+
+    @Column({ type: 'timestamptz' })
+    dateDeCreation!: Date
+
+    @Column({ type: 'timestamptz' })
+    dateDeModification!: Date
+
+    @Column({ type: 'timestamptz' })
+    dateDeSuppression!: Date
+}
