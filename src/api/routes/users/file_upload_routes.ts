3599fcdae1665be04b29d7bb0c36289a171e58d0
John <follyjohn@outlook.fr>
2023-01-24 23:49:01 +0100

    - wip file upload

--- src/api/routes/users/file_upload_routes.ts
+++ src/api/routes/users/file_upload_routes.ts
@@ -4,14 +4,29 @@ import { FilesUploadController } from '../../controllers/filles_uploas_controlle
 
 const fileUploadRouter = express.Router();
 
+
+fileUploadRouter.post('/upload', async (req: express.Request, res) => {
     // #swagger.tags = ['File']
 	// #swagger.summary = 'Uploade new file'
 	// #swagger.description = 'Endpoint to upload new files'
-fileUploadRouter.post('/upload/:containerName/:fileName', async (req: express.Request, res) => FilesUploadController.uploadFile(req, res));
+    // #swagger.parameters['file'] = { description: 'File to upload', in: 'formData', required: true, type: 'file' }
+    /* #swagger.parameters['obj'] = {
+    description: 'User object',
+    in: 'body', required: true,
+    type: 'object',
+    schema: { container_name: 'container_name',
+                file_name: 'file_name' }
+     } */
 
+    FilesUploadController.uploadFile(req, res)
+});
+
+
+fileUploadRouter.delete('/delete', async (req: express.Request, res) => {
     // #swagger.tags = ['File']
 	// #swagger.summary = 'Delete file'
 	// #swagger.description = 'Endpoint to delete files'
-fileUploadRouter.delete('/delete/:containerName/:fileName', async (req: express.Request, res) => FilesUploadController.deleteFile(req, res));
+    FilesUploadController.deleteFile(req, res)
+});
 
 export const fileUploadRoutes = fileUploadRouter;
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
+import multer, {Multer} from 'multer';
+
 
 import { cors_config } from './config/cors';
 
@@ -11,12 +13,16 @@ const userRoutes = require('./api/routes/users/user_routes');
 const authRoutes = require('./api/routes/users/auth_routes');
 import { fileUploadRoutes } from './api/routes/users/file_upload_routes';
 
+const upload = multer();
+
+
 const app: Express = express();
 
 app.use(morgan('[:date[web]] " :method :url " :status :response-time ms :res[content-length]'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.raw({ limit: '50mb' , }));
+app.use(upload.single('file'));
 
 app.use(cors(cors_config));
 
