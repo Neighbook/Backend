078079dc5c553a20cd6deddbf065070bfae1b495
John <follyjohn@outlook.fr>
2023-01-24 14:24:53 +0100

    - added storage blob client utils

---
+++ src/core/azure/blob_storage_client.ts
@@ -0,0 +1,22 @@
+import { ClientSecretCredential } from '@azure/identity';
+import { BlobServiceClient } from '@azure/storage-blob';
+
+import { environnement } from '../../config/environnement';
+
+const key_vault_uri = environnement.azure.key_vault_uri;
+
+let client = null;
+try {
+    client = new BlobServiceClient(
+        key_vault_uri,
+        new ClientSecretCredential(
+            environnement.azure.tenant_id,
+            environnement.azure.client_id,
+            environnement.azure.client_secret
+        )
+    );
+} catch (error) {
+    console.log(`Error while creating vault client: ${error}`);
+}
+
+export const blob_storage_client = client;
