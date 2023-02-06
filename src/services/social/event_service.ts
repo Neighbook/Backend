25d23d70cd0c740d18ca358493a1709d63043eb3
Nathan Kroichvili <nathan.kroichvili@al-enterprise.com>
2023-02-06 14:11:00 +0100

    fix lint error

--- src/services/social/event_service.ts
+++ src/services/social/event_service.ts
@@ -17,20 +17,16 @@ export class EventService {
 			})
 			.then((result) => {
 				event = result;
-			})
-			.catch((error: Error) => {
-				console.log('Error: ' + error);
 			});
 		return event;
 	}
 
-	static async createEvent(title: string, eventDate: Date, location: string) {
+	static async createEvent(title: string, eventDate: Date, location: string): Promise<Event | null> {
 		const event = new Event();
 		let response: Event | null = null;
 		event.titre = title;
 		event.dateEvenement = eventDate;
 		event.addresse = location;
-		console.log('service log : ' + title);
 
 		await this.repository.save(event).then((event) => {
 			response = event;
@@ -38,7 +34,8 @@ export class EventService {
 		return response;
 	}
 
-	static async updateEvent(id: number, title?: string, eventDate?: Date, location?: string) {
+	static async updateEvent(id: number, title?: string, eventDate?: Date, location?: string)
+        :Promise<UpdateResult | null> {
 		const event = new Event();
 		let response: UpdateResult | null = null;
 
@@ -50,24 +47,18 @@ export class EventService {
 			.update(id, event)
 			.then((res) => {
 				response = res;
-			})
-			.catch((error) => {
-				console.log('Error: ' + error);
 			});
 
 		return response;
 	}
 
-	static async deleteEvent(id: number) {
+	static async deleteEvent(id: number): Promise<DeleteResult | null> {
 		let response: DeleteResult | null = null;
 
 		await this.repository
 			.softDelete(id)
 			.then((res) => {
 				response = res;
-			})
-			.catch((error) => {
-				console.log('Error: ' + error);
 			});
 		return response;
 	}
