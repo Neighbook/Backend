0b44eb4942c9a4a14e6ad9de62ab88e76c2954fa
John <follyjohn@outlook.fr>
2023-02-14 16:49:19 +0100

    - fix build

--- src/server.ts
+++ src/server.ts
@@ -12,42 +12,42 @@ const logger = new Logger({ ...ts_logconfig, name: 'Server' });
 dotenv.config();
 
 const normalizePort = (val: string): number | boolean => {
-    const port = parseInt(val, 10);
+	const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return Number(val);
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
+	if (isNaN(port)) {
+		return Number(val);
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
 };
 
 let port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const errorHandler = (error: { syscall: string; code: any }): void => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            logger.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            logger.error(bind + ' is already in use.');
-            // Sugesstion: use a different port
-            logger.info('We will try to use a different port');
-            port = Number(port) + 1;
-            app.set('port', port);
-            server.listen(port);
-            break;
-        default:
-            throw error;
-    }
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+	const address = server.address();
+	const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+	switch (error.code) {
+		case 'EACCES':
+			logger.error(bind + ' requires elevated privileges.');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			logger.error(bind + ' is already in use.');
+			// Sugesstion: use a different port
+			logger.info('We will try to use a different port');
+			port = Number(port) + 1;
+			app.set('port', port);
+			server.listen(port);
+			break;
+		default:
+			throw error;
+	}
 };
 
 VaultService.initialize();
@@ -57,25 +57,25 @@ const server = http.createServer(app);
 server.listen(port);
 
 try {
-    (async () => {
-        await UsersDataSource.initialize();
-        await SocialDataSource.initialize();
-    })();
-    if (!UsersDataSource.isInitialized) {
-        logger.error('Unable to connect to the database ' + UsersDataSource.options.database);
-        throw new Error('Unable to connect to the database ' + UsersDataSource.options.database);
-    }
-    if (!SocialDataSource.isInitialized) {
-        logger.error('Unable to connect to the database ' + SocialDataSource.options.database);
-        throw new Error('Unable to connect to the database ' + SocialDataSource.options.database);
-    }
-    logger.info('Connection with database has been established successfully.');
-    server.on('error', errorHandler);
-    server.on('listening', () => {
-        const address = server.address();
-        const bind = typeof address === 'string' ? 'pipe ' + address : '' + port;
-        logger.info(`⚡️[server]: Server is running at http://localhost:${bind}`);
-    });
+	(async () => {
+		await UsersDataSource.initialize();
+		await SocialDataSource.initialize();
+	})();
+	if (!UsersDataSource.isInitialized) {
+		logger.error('Unable to connect to the database ' + UsersDataSource.options.database);
+		throw new Error('Unable to connect to the database ' + UsersDataSource.options.database);
+	}
+	if (!SocialDataSource.isInitialized) {
+		logger.error('Unable to connect to the database ' + SocialDataSource.options.database);
+		throw new Error('Unable to connect to the database ' + SocialDataSource.options.database);
+	}
+	logger.info('Connection with database has been established successfully.');
+	server.on('error', errorHandler);
+	server.on('listening', () => {
+		const address = server.address();
+		const bind = typeof address === 'string' ? 'pipe ' + address : '' + port;
+		logger.info(`⚡️[server]: Server is running at http://localhost:${bind}`);
+	});
 } catch (error) {
-    logger.error('Unable to connect to the databases:\n', error);
+	logger.error('Unable to connect to the databases:\n', error);
 }
