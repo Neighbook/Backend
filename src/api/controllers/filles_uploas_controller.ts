2316167279cca6482dcf49e2a111241841712f3c
Kroissan <nathankr@free.fr>
2023-05-23 13:51:34 +0200

    fix file upload

--- src/api/controllers/filles_uploas_controller.ts
+++ src/api/controllers/filles_uploas_controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
 
+import { environnement } from '../../config/environnement';
 import { StorageService } from '../../services/users_service/storage_service';
 
 export class FilesUploadController {
 	static async uploadFile(req: Request, res: Response): Promise<void> {
-		const containerName = req.params.container_name;
 		const fileName = req.params.file_name;
 		const content = req.file;
 		if (!content) {
@@ -12,12 +12,12 @@ export class FilesUploadController {
 			return;
 		}
 
-		StorageService.createFile(containerName, fileName, content.buffer).then((value) => {
+		StorageService.createFile(environnement.storage.bucket, fileName, content.buffer).then((value) => {
 			if (!value) {
 				res.status(500).json({ error: 'Error while creating file' });
 				return;
 			}
-			StorageService.get_sas_url(containerName, fileName).then((value) => {
+			StorageService.get_sas_url(environnement.storage.bucket, fileName).then((value) => {
 				if (!value) {
 					res.status(500).json({ error: 'Error while getting file url' });
 					return;
@@ -28,14 +28,9 @@ export class FilesUploadController {
 	}
 
 	static async getFile(req: Request, res: Response): Promise<void> {
-		const containerName = req.params.container_name;
 		const fileName = req.params.file_name;
 
-		if (!(await StorageService.isContainerExist(containerName))) {
-			res.status(500).json({ error: 'Container doesnt exist' });
-			return;
-		}
-		StorageService.get_sas_url(containerName, fileName).then((value) => {
+		StorageService.get_sas_url(environnement.storage.bucket, fileName).then((value) => {
 			if (!value) {
 				res.status(500).json({ error: 'Error while getting file url' });
 				return;
@@ -45,9 +40,8 @@ export class FilesUploadController {
 	}
 
 	static async deleteFile(req: Request, res: Response): Promise<void> {
-		const containerName = req.params.container_name;
 		const fileName = req.params.file_name;
-		const result = await StorageService.deleteFile(containerName, fileName);
+		const result = await StorageService.deleteFile(environnement.storage.bucket, fileName);
 		if (result) {
 			res.status(200).send();
 		} else {
