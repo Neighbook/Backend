0c9fe599344c9732768ed926c96eaa13e6b6bce4
Kroissan <nathankr@free.fr>
2023-05-24 10:26:14 +0200

    revert swagger

--- src/doc/swagger.ts
+++ src/doc/swagger.ts
@@ -2,7 +2,7 @@ import swaggerAutogen from 'swagger-autogen';
 
 import { metadata } from '../config/openapi';
 
-const outputFile = './dist/doc/openapi.json';
+const outputFile = './openapi.json';
 const endpointsFiles = ['../api/routes/*/*.js', '../api/routes/*/*.ts'];
 
 swaggerAutogen(outputFile, endpointsFiles, metadata);
