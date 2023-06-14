a0ed22120e8aeb6b5b96b86e95c34bce96c397e4
MPierre01 <80034983+MPierre01@users.noreply.github.com>
2023-06-14 20:52:35 +0200

    Changements suivi/blocage utilisateurs (#76)
    Changements côté suivi et blocage des utilisateurs :
    
    - Il n'est plus possible de se suivre ou de se bloquer soi-même
    (cohérence avec le Frontend)
    - Lors d'un blocage, les 2 partis concernés arrêtent de se suivre (au
    lieu du bloqueur seulement)

--- src/models/social/Follow.ts
+++ src/models/social/Follow.ts
@@ -1,6 +1,7 @@
-import { Entity, PrimaryColumn } from 'typeorm';
+import { Entity, Check, PrimaryColumn } from 'typeorm';
 
 @Entity('UtilisateurSuivis')
+@Check('"idUtilisateur" != "idUtilisateurSuivi"')
 export class Follow {
 	@PrimaryColumn()
 	idUtilisateur!: string;
